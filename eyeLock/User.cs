ac51ed4e8b24efbe310a7a172f96215d33ef9e5c
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2025-07-23 03:43:54 +0330

    User => Develop User Variables

---
+++ eyeLock/User.cs
@@ -0,0 +1,26 @@
+﻿using eyeStarClassLibrary;
+using System;
+
+namespace eyeLock
+{
+    class User
+    {
+        public static string _Username { get; } = "eyeStar";
+        public static string _Password { get; } = "Hadi1313";
+        public static string _LicenseKey { get; } = "Fj7YP6OcXeKetQWdgyT2/7bEX4ipbN9ImW5Rq/WVltE=";
+        public static string _16ByteKey { get; } = eyeLicense.GtKySxTnBt(_LicenseKey);
+        public static DateTime _Expiration { get; } = new DateTime(2025, 7, 23);
+
+        public static bool CheckLicense()
+        {
+            //return eyeValidation.CheckUSBLicense(_LicenseKey, Global._GlobalKey);
+            return eyeValidation.CheckDeviceLicense(_LicenseKey, Global._GlobalKey);
+            //return eyeValidation.CheckUserLicense(_LicenseKey, Global._GlobalKey);
+        }
+
+        public static bool CheckExpiration()
+        {
+            return eyeValidation.IsSoftwareExpired(_Expiration);
+        }
+    }
+}
