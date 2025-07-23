9038c3407bc762dda5def6f0c945894ca6075dbf
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2025-07-23 03:43:29 +0330

    Global => Develop Global Variable Class

---
+++ eyeLock/Global.cs
@@ -0,0 +1,9 @@
+﻿using eyeStarClassLibrary;
+
+namespace eyeLock
+{
+    class Global
+    {
+        public static string _GlobalKey { get; } = eyeKeyMethods.HMethod(eyeKeyMethods.HApproach("Hadi"));
+    }
+}
