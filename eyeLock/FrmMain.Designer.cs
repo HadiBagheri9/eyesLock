f9ed396b0da9b06a2a7f51a4e7c4cb1df8ee9cc0
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2025-09-07 17:09:08 +0330

    FrmMain -> Development (Last Design Modification)

---
+++ eyeLock/FrmMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace eyeLock
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnUnlockDecrypt = new System.Windows.Forms.Button();
+            this.btnLockEncrypt = new System.Windows.Forms.Button();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkRecoveryFile = new System.Windows.Forms.CheckBox();
+            this.chkFolderAccessibility = new System.Windows.Forms.CheckBox();
+            this.chkCryptography = new System.Windows.Forms.CheckBox();
+            this.rtxtPath = new System.Windows.Forms.RichTextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.diaSelectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlSideBar.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSideBar.Controls.Add(this.btnUnlockDecrypt);
+            this.pnlSideBar.Controls.Add(this.btnLockEncrypt);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 476);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(488, 104);
+            this.pnlSideBar.TabIndex = 0;
+            // 
+            // btnUnlockDecrypt
+            // 
+            this.btnUnlockDecrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(123)))), ((int)(((byte)(193)))));
+            this.btnUnlockDecrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUnlockDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUnlockDecrypt.Location = new System.Drawing.Point(33, 24);
+            this.btnUnlockDecrypt.Name = "btnUnlockDecrypt";
+            this.btnUnlockDecrypt.Size = new System.Drawing.Size(132, 54);
+            this.btnUnlockDecrypt.TabIndex = 0;
+            this.btnUnlockDecrypt.Text = "Unlock|Dec";
+            this.btnUnlockDecrypt.UseVisualStyleBackColor = false;
+            this.btnUnlockDecrypt.Click += new System.EventHandler(this.btnUnlockDecrypt_Click);
+            // 
+            // btnLockEncrypt
+            // 
+            this.btnLockEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(82)))), ((int)(((byte)(164)))));
+            this.btnLockEncrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLockEncrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLockEncrypt.Location = new System.Drawing.Point(322, 24);
+            this.btnLockEncrypt.Name = "btnLockEncrypt";
+            this.btnLockEncrypt.Size = new System.Drawing.Size(132, 54);
+            this.btnLockEncrypt.TabIndex = 0;
+            this.btnLockEncrypt.Text = "Lock|Enc";
+            this.btnLockEncrypt.UseVisualStyleBackColor = false;
+            this.btnLockEncrypt.Click += new System.EventHandler(this.btnLockEncrypt_Click);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Controls.Add(this.pictureBox1);
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Controls.Add(this.chkRecoveryFile);
+            this.pnlMain.Controls.Add(this.chkFolderAccessibility);
+            this.pnlMain.Controls.Add(this.chkCryptography);
+            this.pnlMain.Controls.Add(this.rtxtPath);
+            this.pnlMain.Controls.Add(this.btnSelectFolder);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(488, 476);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "eyeLock";
+            // 
+            // chkRecoveryFile
+            // 
+            this.chkRecoveryFile.AutoSize = true;
+            this.chkRecoveryFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkRecoveryFile.Location = new System.Drawing.Point(33, 409);
+            this.chkRecoveryFile.Name = "chkRecoveryFile";
+            this.chkRecoveryFile.Size = new System.Drawing.Size(233, 29);
+            this.chkRecoveryFile.TabIndex = 2;
+            this.chkRecoveryFile.Text = "Create Recovery File";
+            this.chkRecoveryFile.UseVisualStyleBackColor = true;
+            this.chkRecoveryFile.CheckedChanged += new System.EventHandler(this.chkRecoveryFile_CheckedChanged);
+            // 
+            // chkFolderAccessibility
+            // 
+            this.chkFolderAccessibility.AutoSize = true;
+            this.chkFolderAccessibility.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkFolderAccessibility.Location = new System.Drawing.Point(33, 362);
+            this.chkFolderAccessibility.Name = "chkFolderAccessibility";
+            this.chkFolderAccessibility.Size = new System.Drawing.Size(217, 29);
+            this.chkFolderAccessibility.TabIndex = 2;
+            this.chkFolderAccessibility.Text = "Folder Accessibility";
+            this.chkFolderAccessibility.UseVisualStyleBackColor = true;
+            this.chkFolderAccessibility.CheckedChanged += new System.EventHandler(this.chkFolderAccessibility_CheckedChanged);
+            // 
+            // chkCryptography
+            // 
+            this.chkCryptography.AutoSize = true;
+            this.chkCryptography.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkCryptography.Location = new System.Drawing.Point(33, 317);
+            this.chkCryptography.Name = "chkCryptography";
+            this.chkCryptography.Size = new System.Drawing.Size(160, 29);
+            this.chkCryptography.TabIndex = 2;
+            this.chkCryptography.Text = "Cryptography";
+            this.chkCryptography.UseVisualStyleBackColor = true;
+            this.chkCryptography.CheckedChanged += new System.EventHandler(this.chkCryptography_CheckedChanged);
+            // 
+            // rtxtPath
+            // 
+            this.rtxtPath.Location = new System.Drawing.Point(33, 159);
+            this.rtxtPath.Name = "rtxtPath";
+            this.rtxtPath.ReadOnly = true;
+            this.rtxtPath.Size = new System.Drawing.Size(421, 118);
+            this.rtxtPath.TabIndex = 1;
+            this.rtxtPath.Text = "";
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(41)))));
+            this.btnSelectFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelectFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectFolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.btnSelectFolder.Location = new System.Drawing.Point(33, 46);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(421, 83);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = false;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(282, 289);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 172);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(488, 580);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSideBar);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmMain";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.pnlSideBar.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Button btnUnlockDecrypt;
+        private System.Windows.Forms.Button btnLockEncrypt;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkRecoveryFile;
+        private System.Windows.Forms.CheckBox chkFolderAccessibility;
+        private System.Windows.Forms.CheckBox chkCryptography;
+        private System.Windows.Forms.RichTextBox rtxtPath;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.FolderBrowserDialog diaSelectFolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ eyeLock/FrmMain.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+using eyeStarClassLibrary;
+using System.Collections.Generic;
+using PersonalClassLibrary.Notif;
+using PersonalClassLibrary.Windows;
+using System.Windows.Forms;
+
+namespace eyeLock
+{
+    public partial class FrmMain : FrmTemp
+    {
+        List<string> listFiles = new List<string>();
+        string fileNameAddition = "eye";
+        string path;
+        bool isCryptionOn = false, isLockingOn = false, isRecoveryFileOn = false;
+
+        public FrmMain()
+        {
+            if (User.CheckLicense())
+            {
+                if (!User.CheckExpiration())
+                {
+                    InitializeComponent();
+                    this.SetTheme();
+                }
+                else
+                {
+                    eyeMessageContents.LicenseExpired.MessageBoxError();
+                    Environment.Exit(0);
+                }
+            }
+            else 
+            {
+                eyeMessageContents.LicenseInvalid.MessageBoxError();
+                Environment.Exit(0);
+            }
+            
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            DisableComponents();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = diaSelectFolder.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                EnableComponents();
+                DisableButtons();
+            }
+
+            path = diaSelectFolder.SelectedPath;
+            rtxtPath.Text = path;
+        }
+
+        private void btnLockEncrypt_Click(object sender, EventArgs e)
+        {
+            if (isCryptionOn)
+            {
+                EncryptFiles(path);
+            }
+
+            if (isLockingOn)
+            {
+                LockFolder(path);
+            }
+
+            if (isRecoveryFileOn)
+            {
+                BackUpFile(path);
+            }
+
+            "Done".MessageBoxWarning();
+        }
+
+        private void btnUnlockDecrypt_Click(object sender, EventArgs e)
+        {
+            if (isCryptionOn)
+            {
+                DecryptFiles(path);
+            }
+
+            if (isLockingOn)
+            {
+                UnlockFolder(path);
+            }
+
+            "Done".MessageBoxInformation();
+        }
+
+        private void chkCryptography_CheckedChanged(object sender, EventArgs e)
+        {
+            isCryptionOn = chkCryptography.Checked ? true : false;
+            CheckAndChangeButtonsAbility();
+        }
+
+        private void chkFolderAccessibility_CheckedChanged(object sender, EventArgs e)
+        {
+            isLockingOn = chkFolderAccessibility.Checked ? true : false;
+            CheckAndChangeButtonsAbility();
+        }
+
+        private void chkRecoveryFile_CheckedChanged(object sender, EventArgs e)
+        {
+            isRecoveryFileOn = chkRecoveryFile.Checked ? true : false;
+            CheckAndChangeButtonsAbility();
+        }
+
+        //**********************************************************************
+
+        private void LockFolder(string path)
+        {
+            try
+            {
+                FolderOptions.LockDirectory(path);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxError();
+            }
+        }
+
+        private void UnlockFolder(string path)
+        {
+            try
+            {
+                FolderOptions.UnLockDirectory(path);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxError();
+            }
+        }
+
+        private void EncryptFiles(string path)
+        {
+            try
+            {
+                listFiles = FolderOptions.GetAllFiles(path);
+
+                foreach (var item in listFiles)
+                {
+                    PersonalClassLibrary.Windows.FileOptions.EncryptFile(item, item + fileNameAddition, User._16ByteKey);
+                    File.Delete(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxError();
+            }
+        }
+
+        private void DecryptFiles(string path)
+        {
+            try
+            {
+                listFiles = FolderOptions.GetAllFiles(path);
+
+                foreach (var item in listFiles)
+                {
+                    if (item.EndsWith(fileNameAddition))
+                    {
+                        PersonalClassLibrary.Windows.FileOptions.DecryptFile(item, item.Remove(item.Length-3, 3), User._16ByteKey);
+                        File.Delete(item);
+                    }
+                    else
+                    {
+                        PersonalClassLibrary.Windows.FileOptions.DecryptFile(item, item + fileNameAddition, User._16ByteKey);
+                        File.Delete(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxError();
+            }
+        }
+
+        private void BackUpFile(string path)
+        {
+             
+        }
+
+        //**********************************************************************
+
+        private void CheckAndChangeButtonsAbility()
+        {
+            if ((!isCryptionOn) && (!isLockingOn))
+            {
+                DisableButtons();
+            }
+            else
+            {
+                EnableButtons();
+            }
+        }
+
+        /// <summary>
+        /// To disable components
+        /// </summary>
+        private void DisableComponents()
+        {
+            chkCryptography.Enabled = false;
+            chkFolderAccessibility.Enabled = false;
+            chkRecoveryFile.Enabled = false;
+            btnLockEncrypt.Enabled = false;
+            btnUnlockDecrypt.Enabled = false;
+        }
+
+        /// <summary>
+        /// To enable components
+        /// </summary>
+        private void EnableComponents()
+        {
+            chkCryptography.Enabled = true;
+            chkFolderAccessibility.Enabled = true;
+            chkRecoveryFile.Enabled = true;
+            btnLockEncrypt.Enabled = true;
+            btnUnlockDecrypt.Enabled = true;
+        }
+
+        /// <summary>
+        /// To disable buttons
+        /// </summary>
+        private void DisableButtons()
+        {
+            btnLockEncrypt.Enabled = false;
+            btnUnlockDecrypt.Enabled = false;
+        }
+
+        /// <summary>
+        /// To enable buttons
+        /// </summary>
+        private void EnableButtons()
+        {
+            btnLockEncrypt.Enabled = true;
+            btnUnlockDecrypt.Enabled = true;
+        }
+    }
+}
