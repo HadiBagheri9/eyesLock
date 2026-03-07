ebac865ee203ededc58ef6deed6b0c9dfcaf6387
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-03-07 18:12:49 +0330

    Clean Code -> Add Variables to Global.cs

--- eyeLock/Global.cs
+++ eyeLock/Global.cs
@@ -5,5 +5,9 @@ namespace eyeLock
     class Global
     {
         public static string _LKE_DK { get; } = eye_Key_IV.HMethod_DK(eye_Key_IV.HApproach("Hadi"));
+        public static string _FE_Base { get; set; }
+        public static string _FE_Bridge { get; set; }
+        public static string _FE_DK { get; set; }
+        public static byte[] _FE_DV { get; set; } = new byte[16];
     }
 }
