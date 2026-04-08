7b52440f2f58c7a87f5eb31bb1ce3705c6494702
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-04-08 14:41:03 +0330

    List SeedPhrases

--- eyesLock/FileOptions.cs
+++ eyesLock/FileOptions.cs
@@ -1,11 +1,26 @@
 ﻿using System.IO;
-using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace eyesLock
 {
     class FileOptions
     {
+        static string seedPhraseFileFormat = ".eyesph";
+
+        public static List<string> ListSeedPhrases()
+        {
+            List<string> listFiles = new List<string>();
+            string[] arrayFiles = Directory.GetFiles(Application.StartupPath, $"*{seedPhraseFileFormat}");
+            foreach (var item in arrayFiles)
+            {
+                listFiles.Add(item);
+            }
+
+            return listFiles;
+        }
         public static bool EncryptFile(string inputFilePath, string outputFilePath, string key, byte[] iv)
         {
             using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
--- eyesLock/Program.cs
+++ eyesLock/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace eyesLock
 {
     static class Program
     {
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -13,7 +17,29 @@ namespace eyesLock
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            //Application.Run(new FrmMain());
+
+            
+
+            //The initial version of the software includes only one seed phrase file.
+            //The ability to add more seed phrases will be added soon.
+            //If more than one seed phrase file exist, it opens first item of the list.
+            if (listFiles.Count > 0)
+            {
+                //FrmEnterPass frmEnterPass = new FrmEnterPass();
+            }
+            else
+            { 
+                //Open FrmImportSeed
+            }
+
+            string testM = "";//1
+            foreach (var item in listFiles)//1
+            {
+                testM += item + "\n";//1
+            }
+            MessageBox.Show(testM);//1
+            
         }
     }
 }
