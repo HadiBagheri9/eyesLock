0514af6ef43b22211f23290c98adff18b0c4de7f
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-04-22 06:30:18 +0330

    Define CryptText Class

---
+++ eyesLock/CryptText.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace eyesLock
+{
+    class CryptText
+    {
+        public static string Encrypt(string text, string key, byte[] iv)
+        {
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(text);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        /// <summary>
+        /// To decrypt a string of text.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Decrypt(string text, string key, byte[] iv)
+        {
+            byte[] buffer = Convert.FromBase64String(text);
+
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 256;
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = iv;
+
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (MemoryStream memoryStream = new MemoryStream(buffer))
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                            {
+                                return streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxWarning();
+                return "";
+            }
+        }
+    }
+}
