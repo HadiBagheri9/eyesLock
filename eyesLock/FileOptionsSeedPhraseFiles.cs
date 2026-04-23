8b19265728efe2eeaa84ab4ac81a4096df50639e
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-04-23 06:34:34 +0330

    FileOptionsSeedPhraseFiles -> Debug the 13th mnemonic phrase

--- eyesLock/FileOptionsSeedPhraseFiles.cs
+++ eyesLock/FileOptionsSeedPhraseFiles.cs
@@ -23,15 +23,43 @@ namespace eyesLock
             return listFiles;
         }
 
+        /// <summary>
+        /// Get 12 Seed Phrases and return them as a String Array
+        /// </summary>
+        /// <param name="seedPhraseVariable"></param>
+        /// <param name="seedPhraseContent"></param>
+        /// <returns></returns>
         public static string[] Get12SeedPhrases(this string[] seedPhraseVariable, string seedPhraseContent)
         {
-            string[] getContents = seedPhraseContent.Split(Global._SeedPhraseSeparator[0]);
+            string[] getContents = seedPhraseContent.Split(Global._SeedPhraseSeparator);
 
             for (int i = 0; i < seedPhraseVariable.Length; i++)
             {
                 seedPhraseVariable[i] = getContents[i].Trim();
             }
             return seedPhraseVariable;
+        }
+
+        /// <summary>
+        /// Get the 13th Seed Phrase and return it as a String Variable
+        /// </summary>
+        /// <param name="seedPhraseVariable"></param>
+        /// <param name="seedPhraseContent"></param>
+        /// <returns></returns>
+        public static string Get13thSeedPhrase(this string seedPhraseVariable, string seedPhraseContent)
+        {
+            seedPhraseVariable = "";
+            string[] getContents = seedPhraseContent.Split(Global._SeedPhraseSeparator);
+
+            if (getContents.Length > 12)
+            {
+                for (int i = 12; i < getContents.Length; i++)
+                {
+                    seedPhraseVariable += getContents[i];
+                }
+            }
+
+            return seedPhraseVariable.Trim();
         }
     }
 }
