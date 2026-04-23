56f60876b2db624337f16b5b5e2450f657c8cd45
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-04-23 14:49:43 +0330

    Customized TextBox

---
+++ eyesLock/System/Windows/Forms/ThemeTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace System.Windows.Forms
+{
+    internal class ThemeTextBox : TextBox
+    {
+        public Color BorderColor { get; set; } = Color.Gray;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0xF || m.Msg == 0x133)
+            {
+                using (Graphics g = Graphics.FromHwnd(Handle))
+                {
+                    Rectangle rect = new Rectangle(0, 0, Width - 1, Height - 1);
+                    ControlPaint.DrawBorder(g, rect, BorderColor, ButtonBorderStyle.Solid);
+                }
+            }
+        }
+    }
+}
