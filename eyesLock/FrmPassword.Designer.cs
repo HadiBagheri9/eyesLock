c1b147478a690a685f1fd1d80f4af1b7359216f4
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-04-23 07:08:00 +0330

    FrmPassword -> Implemention

---
+++ eyesLock/FrmPassword.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace eyesLock
+{
+    partial class FrmPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkShowPassword = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSideBar.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSideBar.Controls.Add(this.btnClose);
+            this.pnlSideBar.Controls.Add(this.btnOK);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 213);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(488, 104);
+            this.pnlSideBar.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(41)))));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.btnClose.Location = new System.Drawing.Point(33, 24);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(132, 54);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(82)))), ((int)(((byte)(164)))));
+            this.btnOK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Location = new System.Drawing.Point(322, 24);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(132, 54);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Controls.Add(this.label3);
+            this.pnlMain.Controls.Add(this.chkShowPassword);
+            this.pnlMain.Controls.Add(this.label2);
+            this.pnlMain.Controls.Add(this.txtTitle);
+            this.pnlMain.Controls.Add(this.txtPassword);
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(488, 213);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Title :";
+            // 
+            // chkShowPassword
+            // 
+            this.chkShowPassword.AutoSize = true;
+            this.chkShowPassword.Location = new System.Drawing.Point(33, 164);
+            this.chkShowPassword.Name = "chkShowPassword";
+            this.chkShowPassword.Size = new System.Drawing.Size(184, 29);
+            this.chkShowPassword.TabIndex = 1;
+            this.chkShowPassword.Text = "Show Password";
+            this.chkShowPassword.UseVisualStyleBackColor = true;
+            this.chkShowPassword.CheckedChanged += new System.EventHandler(this.chkShowPassword_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(209, 60);
+            this.txtTitle.MaxLength = 32;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(245, 31);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(209, 113);
+            this.txtPassword.MaxLength = 32;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(245, 31);
+            this.txtPassword.TabIndex = 0;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 317);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSideBar);
+            this.Name = "FrmPassword";
+            this.Text = "eyes\'Lock";
+            this.Load += new System.EventHandler(this.FrmPassword_Load);
+            this.pnlSideBar.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkShowPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+    }
+}
---
+++ eyesLock/FrmPassword.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace eyesLock
+{
+    public enum FrmPasswordType { Set, Enter};
+    
+    public partial class FrmPassword : FrmTemp
+    {
+        public string _Password, _Title;
+        private FrmPasswordType? frmPasswordType = null;
+        public FrmPassword(FrmPasswordType frmPasswordType)
+        {
+            InitializeComponent();
+            this.SetTheme();
+            this.frmPasswordType = frmPasswordType;
+        }
+
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !chkShowPassword.Checked;
+        }
+
+        private void FrmPassword_Load(object sender, EventArgs e)
+        {
+            label2.Text = frmPasswordType.ToString() + " " + "Password : ";
+            btnOK.Text = frmPasswordType.ToString();
+
+            txtTitle.Enabled = (frmPasswordType == FrmPasswordType.Set);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            _Password = null;
+            Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // 1 Needs Conditions
+            if (frmPasswordType == FrmPasswordType.Set)
+            {
+                _Password = txtPassword.Text.Trim();
+                _Title = txtTitle.Text.Trim();
+
+                Close();
+            }
+            else
+            {
+                _Password = txtPassword.Text.Trim();
+
+                Close();
+            }
+        }
+    }
+}
