a1baa57433f6f65fd1e244e4e95cf7a2c537e541
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-06-09 17:33:16 +0330

    Set Assembly Info

--- eyesLock/Properties/AssemblyInfo.cs
+++ eyesLock/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("eyesLock")]
-[assembly: AssemblyDescription("LUX NASCITUR IN TENEBRIS")]
+[assembly: AssemblyDescription("A software to protect your files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("eyeStar")]
 [assembly: AssemblyProduct("eyesLock")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("277054d3-0714-4f49-89d8-e3257e7fdde5")]
